0754494c2283d5d50479f9d574d07203716c3df6
Kryptom <tom11032002@gmail.com>
2023-04-28 20:23:02 +0200

    new weapon saving system

---
+++ checkWeapons.py
@@ -0,0 +1,85 @@
+from weapons import WEPS
+import pyautogui
+from PIL import Image, ImageEnhance, ImageGrab, ImageFilter
+import kNearestNeighbors as knn
+from time import sleep
+
+WEAPON_FIELD = [713,1038,950,1064]
+WEAPONPIXELS = []
+
+def loadWeaponPixels():
+    global WEAPONPIXELS_ACTIVE
+    print("----------------------------------------------------------------")
+    print("lade Waffen-Trainings-Daten")
+    print("----------------------------------------------------------------")
+    file1 = open('WeaponPixels.txt', 'r')
+    Lines = file1.readlines()
+
+    count = 0
+    for line in Lines:
+        count += 1
+        txt = "{}".format(line.strip())
+        d = txt.split(" ")
+        for i in range(2,len(d)):
+            d[i] = int(d[i])
+        WEAPONPIXELS.append([d[2:], d[0].replace("#"," "), int(d[1])])
+        if count % 10 == 0: print(f"WAFFEN | {count} von {len(Lines)} geladen")
+
+    WEAPONPIXELS_ACTIVE = True
+    print("----------------------------------------------------------------")
+    print("Trainings-Daten wurden geladen, schalte nun auf bessere Waffenerkennung um.")
+    print("----------------------------------------------------------------")
+    
+def makeScreenFromWeapons():
+    cap = ImageGrab.grab(bbox = (WEAPON_FIELD[0],WEAPON_FIELD[1],WEAPON_FIELD[2],WEAPON_FIELD[3]))
+    filter = ImageEnhance.Color(cap)
+    cap = filter.enhance(50)
+    enhancer = ImageEnhance.Contrast(cap)
+    cap = enhancer.enhance(1)
+    
+    newCap  = Image.new(mode = "RGB", size = (cap.width, cap.height), color = (0, 0, 0))
+    for x in range(cap.width):
+        for y in range(cap.height):
+            color = cap.getpixel((x, y))
+            if color[0] >= 230 and color[1] >= 230 and color[2] >= 230: 
+                newCap.putpixel((x,y),(255,255,255))
+            else:
+                newCap.putpixel((x,y),(0,0,0))
+    return newCap
+
+def convertTo1DArray(cap):
+    arr = []
+    num = 0
+    for x in range(cap.width):
+        for y in range(cap.height):
+            color = cap.getpixel((x, y))
+            c = round(0.2989 * color[0] + 0.5870 * color[1] + 0.1140 * color[2])
+            if c == 255:
+                arr.append(1)
+                num += 1
+            else: arr.append(0)
+    return arr, num
+
+def getSelectedWeaponNeighbors():
+    cap = makeScreenFromWeapons()
+    arr, ones = convertTo1DArray(cap)
+    new_point = arr
+    wep_str = knn.multiThreadfindCategory(new_point, WEAPONPIXELS, 8, ones, fixedK=1)
+    
+    for wep_cat in WEPS:
+        for wep in WEPS[wep_cat]:
+            if type(wep) is tuple: wep = wep[0]
+            if wep == wep_str: return wep, wep_cat
+    return "shot", "normal"
+
+if __name__ == "__main__":
+    loadWeaponPixels()
+    sleep(3)
+    while True:
+        wep_data = getSelectedWeaponNeighbors()
+        
+        print(wep_data)
+        
+        pyautogui.keyDown("w")
+        pyautogui.keyUp("w")
+        sleep(0.2)
--- mainMK9.py
+++ mainMK9.py
@@ -12,6 +12,7 @@ import queue
 from PIL import Image, ImageEnhance, ImageGrab, ImageFilter
 from datetime import datetime
 from threading import Thread 
+from weapons import WEPS
 
 RULES = """
         ~~~DAS IST EIN BILLIGER FARMBOT FÜR XP~~~
@@ -57,92 +58,6 @@ MyTank = (0, 220, 15)
 EnemyTank = (194,3,3)
 Gear = (244,245,126)
 Terrain = (64,217,255)
-
-#Waffen
-WEPS = {"normal": ["shot", "big shot", "heavy shot", "massive shot", "one-bounce", "three-bounce",
-                    "five-bounce", "seven-bounce", "digger", "mega-digger", "splitter", "double-splitter", "super-splitter",
-                    "breaker", "double-breaker", "super-breaker", "breakerchain", "twinkler", "sparkler",
-                    "crackler", "sniper", "sub-sniper", "smart snipe", "cactus",
-                    "bulger", "big bulger", "sinkhole", "area strike", "area attack", "gatling gun",
-                    "tunneler", "torpedoes", "tunnel strike", "megatunneler", "horizon", "sweeper",
-                    "flower", "bouquet",
-                    "banana", "banana split", "banana bunch", "snake", "python", "cobra", "zipper",
-                    "double zipper", "zipper quad", "dead riser", "pixel", "mega-pixel", "super-pixel", "ultra-pixel",
-                    "builder", "megabuilder", "static", "static link", "static chain", "molehill", "moles",
-                    "rainbow", "megarainbow", "mini-pinger", "pinger", "ping flood", "bolt", "lightning", "2012",
-                    "spiker", "super spiker", "boomerang", "bigboomerang",
-                    "mini-v", "flying-v", "yin yang", "yin yang yong", "fireworks", "grand finale",
-                    "pyrotechnics", "water balloon", "magnets", "arrow", "bow & arrow", "flaming arrow",
-                    "driller", "slammer", "quicksand", "desert", "jumper", "triple-jumper", "spaz",
-                    "spazzer", "spaztastic", "bounder", "double bounder", "triple bounder",
-                    "sticky bomb", "mine layer", "napalm", "heavy napalm",
-                    "sunburst", "solar flare", "payload", "magic shower", "shadow", "darkshadow",
-                    "deathshadow", "baby seagull", "seagull", "mama seagull",
-                    "penetrator", "penetrator v2",
-                    "chunklet", "chunker", "bouncy ball", "super ball", "battering ram", "double ram",
-                    "ram-squad", "double ram-squad", "asteroids", "spider", "tarantula", "daddy longlegs",
-                    "black widow", "clover", "four leaf clover", "snowball",
-                    "spotter", "spotter xl", "spotter xxl", "fighter jet", "heavy jet",
-                    "bee hive", "killer bees",
-                    "wasps", "dual-roller", "partition", "division", "bfg-1000", "bfg-9000",
-                    "mini-turret", "nuke", "meganuke",
-                    "black hole", "cosmic rift", "breakermadness", "breakermania", "homing missile",
-                    "homing rockets", "puzzler", "deceiver", "baffler", "pentagram", "pentaslam", "radar",
-                    "sonar", "lidar", "tweeter", "squawker", "woofer", "sunflower",
-                    "helianthus", "sausage", "bratwurst", "kielbasa", "fury", "rage", "relocator",
-                    "displacement bomb", "stone", "boulder", "fireball",
-                    "cat", "supercat", "cats and dogs", "asteroid storm",
-                    "ghost bomb", "flasher", "strobie", "rave", "sprouter", "blossom", "square", "hexagon",
-                    "octagon", "satellite", "ufo", "palm", "double palm", "triple palm", "fountain", 
-                    "waterworks", "sprinkler", "flattener", "wall", "fortress", "funnel", "mad birds",
-                    "furious birds", "livid birds", "beacon", "beaconator", "chopper", "apache", "skullshot",
-                    "skeleton", "hidden blade",
-                    "secret blade", "concealed blade", "portal gun", "ashpd", "volcano", "eruption",
-                    "tangent fire", "tangent attack", "tangent assault", "summoner", "mage",
-                    "travelers", "scavengers", "wicked witch", "witches broom",
-                    "ghouls", "oddball", "oddbomb", "botherer", "tormentor", "punisher", "cropduster", "obnoxiousduster",
-                    "toxicduster", "small potion", "medium potion", "large potion", "huge potion", "wanderer",
-                    "double wanderer", "triple wanderer", "censorbar", "restrictedbar", "redactedbar",
-                    "kiss", "french kiss", "smooch", "confuser", "super-confuser", "mega-confuser"],
-        
-        "straight": ["glock","m9","desert eagle","moons","orbitals","shank","dagger","sword","uzi","mp5","p90",
-                    "rampage","riot","m4","m16","ak-47", "laser beam","great beam","ultra beam","master beam",
-                    "shockshell","shockshell trio","flintlock","blunderbuss",
-                    "fat stacks", "dolla bills", "make-it-rain"],
-
-        "instant": ["earthquake","mega-quake","shockwave","sonic pulse","drone","heavy drone","attractoids",
-                    "imploder", "ultimate imploder", "health aura", "health aura+", "health aura++"],
-
-        "45degrees": ["three-ball", "five-ball", "eleven-ball", "twentyfive-ball", "stream", "creek", "river",
-                    "tsunami", "flame", "blaze", "inferno", "splitterchain", "rapidfire", "shotgun", "burst-fire",
-                    "gattling gun", "guppies", "minnows", "goldfish", "construction", "excavation", "counter 3000",
-                    "counter 4000", "counter 5000", "counter 6000", "gravies", "gravits", "tadpoles", "frogs",
-                    "bullfrog", "water trio", "water fight", "fiesta", "sticky trio", "fleet", "heavy fleet",
-                    "super fleet", "squadron", "sillyballs", "wackyballs", "crazyballs", "synclets", "super-synclets",
-                    "x attack", "o attack", "minions", "many-minions", "asteorids", "comets",
-                    "starfire", "turretmob", "bumper bombs", "bumper array", "bumper assault", "rocket fire",
-                    "rocket cluster", "flurry", "taser", "jackpot", "mega-jackpot", "ultra-jackpot", "lottery",
-                    "kittens", "ghostlets", "wacky cluster", "kooky cluster", "crazy cluster", "chicken-fling",
-                    "chicken-hurl", "chicken-launch", "pepper", "salt and pepper", "paprika", "cayenne", "needler",
-                    "dual needler", "kernels", "popcorn", "burnt popcorn", "skeet", "olympic skeet", "heavy taser",
-                    "jammer", "jiver", "rocker", "bounsplode", "double-bounsplode", "triple-bounsplode", "pongslam", "pyroslam"],
-
-        "deltaAngle": [("roller",-1), ("heavy roller",-1), ("groller",-1), ("back-roller",1),
-                    ("heavy back-roller",1), ("back-groller",1), ("saw blade",-2), ("rip saw",-2), ("diamond blade",-2),
-                    ("dead weight",2), ("permaroller",-3), ("heavy permaroller",-4), ("ringer",-1), ("heavy ringer",-2),
-                    ("olympic ringer",-2), ("hover-ball",-3), ("heavy hover-ball",-3), ("train",-2), ("express",-2),
-                    ("throwing star",-2), ("multi-star",-2), ("ninja",-2)],
-
-        "radius": [("3d-bomb",33), ("2x3d",33), ("3x3d",33), ("early bird",100), ("early worm",100)],
-
-        "landing": ["grenade", "tri-nade", "multi-nade", "grenade storm", "cactus strike","air strike",
-                    "helicopter strike", "ac-130", "artillery", "disco ball", "groovy ball", "pinata",
-                    "sticky rain", "rain", "hail", "carpet bomb", "carpet fire", "incendiary bombs",
-                    "shrapnel", "shredders", "bounstrike", "bounstream", "bounstorm", "jack-o-lantern",
-                    "jack-o-vomit", "recruiter", "enroller", "enlister", "pinpoint", "needles", "pins and needles",
-                    "god rays", "deity", "spreader", "kamikaze", "suicide bomber", "acid rain", "acid hail",
-                    "shooting star", "snowstorm", "firestorm"]
-        }
 
 #Sonstiges
 settings = json.load(open("settings.json"))
