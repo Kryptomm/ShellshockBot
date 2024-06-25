611888d36c3da230bd667b16bf6cf84fb089f4a7
Tom Keller <tom11032002@gmail.com>
2024-06-25 17:22:21 +0200

    deleted where

--- src/where.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def main():
-    # Get the absolute path of the directory where the script is located
-    script_dir = os.path.abspath(os.path.dirname(__file__))
-    print(f"The script is run from: {script_dir}")
-
-if __name__ == "__main__":
-    main()
